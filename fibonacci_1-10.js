1cab2912953d382904945aaab8a70843202bef96
hebiao1973 <18502159@qq.com>
2020-02-24 21:58:30 +0800

    增加了sudoku.js

--- fibonacci_1-10.js
+++ fibonacci_1-10.js
@@ -1,6 +1,18 @@
 //学习JavaScript，列出各种各样的斐波那契数列函数
-const f = n => {
+const f1 = n => {
     if (n == 0) return 0
     else if (n == 1) return 1
-    else return fibonacci(n - 1) + fibonacci(n - 2)
+    else return f1(n - 1) + f1(n - 2)
+}
+
+const f2 = n => {
+    let last = 1
+    let last2 = 0
+    let current = last2
+    for (let i = 1; i <= n; i++) {
+        last2 = last
+        last = current
+        current = last + last2
+    }
+    return current
 }
